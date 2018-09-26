0908758896a29335de6dfe7f934b4a39aad7498e
James Cherven <jcherven@mail.usf.edu>
2018-09-26 14:29:34 -0400

    Adds toggling gifs between animate and still

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,12 @@
 // https://api.giphy.com/v1/gifs/search?api_key=DGO8o3GLH8FdLjs2Orr9XM48VgDnHxf9&limit=5&q=lemur
 const apiKey = 'DGO8o3GLH8FdLjs2Orr9XM48VgDnHxf9';
-const searchResultLimit = 5;
+const searchResultLimit = 2;
 
 // jquery DOM objects
 $addTopicButton = $('#add-topic-button');
 $searchBar = $('#add-topic-searchbar');
-$contentArea = $('#gif-grid')
+$contentArea = $('#gif-grid');
+$gifItem = $('.gif');
 
 $('document').ready(function() {
     // listen for an enter keypress and treat it like a button click for adding a gif topic
@@ -13,10 +14,6 @@ $('document').ready(function() {
         if(event.which == 13) {
             $addTopicButton.click();
         }
-    })
-    // When a gif still is clicked, swap the still url with the animated url
-    $('.gif').on('click', function() {
-        console.log('User clicked ' + $(this).val('class'));
     })
 
     // When a topic search term is added, handle the Giphy API call
@@ -26,42 +23,55 @@ $('document').ready(function() {
         var queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&limit=' + searchResultLimit + '&q=' + searchTerm;
         // clear the user's search term after entering
         $searchBar.val('');
-        $searchBar.attr('placeholder', 'Add another topic');
+        if ( $contentArea.is(':empty') )
+            $searchBar.attr('placeholder', 'Add a topic');
+        else
+            $searchBar.attr('placeholder', 'Add another topic');
     
         $.ajax({
         url: queryUrl,
         method: "GET"
         }).then(function(response) {
-            var responseObject = response.data;
-    
-            var responseKeys = Object.keys(responseObject);
+            var responseObj = response.data;
 
-            // Step through responseObject, create img tags for each image, and present them in $contentArea
-            for ( var key in response.data ) {
-                console.log(response.data[key].images.fixed_height_small_still.url);
-                var stillUrl = response.data[key].images.fixed_height_small_still.url;
+            // Step through responseObj, create img tags for each gif, and present them in $contentArea
+            for ( var key in responseObj ) {
+                var stillUrl = responseObj[key].images.fixed_height_small_still.url;
                 var responseImage = $('<img>');
+                // responseImage.attr('id', key);
+                responseImage.addClass(searchTerm);
+                responseImage.addClass('gif');
+                responseImage.attr('data-state', 'still');
+                responseImage.attr('data-still', responseObj[key].images.fixed_height_small_still.url);
+                responseImage.attr('data-animate', responseObj[key].images.fixed_height_small.url);
                 responseImage.attr('src', stillUrl);
-                responseImage.attr('id', key);
-                responseImage.attr('class', searchTerm);
-                // Append the 'gif' to the class attribute instead of replacing it
-                responseImage.attr('class', function() { return $(this).attr('class') + ' gif'});
+                // Add attributes for still and animated
 
                 $contentArea.prepend(responseImage);
-            }
+            } // End for(key in response.data)
+            
+            // TODO: Add a new topic button
+            
+        }); // End AJAX call
 
-            console.log('responseItems: ' + responseKeys);
-    
-            // 
-    
-            //var responseImage = $("<img>");
-    
-            //responseImage.attr("src", imageUrl);
-            //console.log('responseImage is: ' + responseImage);
-    
-            //$contentArea.prepend(responseImage);
-    
-        });
-})
+        // When a gif is clicked, toggle between animated and still
+        $($contentArea).on('click', '.gif', function() {
+            console.log('User clicked ' + $(this).attr('class'));
+
+            var $gifState = $(this).attr('data-state');
+
+            if ( $(this).attr('data-state') === 'still' ) {
+                $(this).attr('src', $(this).attr('data-animate'));
+                $(this).attr('data-state', 'animate');
+            }
+            else {
+                $(this).attr('src', $(this).attr('data-still'));
+                $(this).attr('data-state', 'still');
+            }
+        }) // End .on(click...)
+
+    }) // End $addTopicButton.on(click...)
+
+
 
 });
